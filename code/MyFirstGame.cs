354d78a4d7dae32b78cb8d6533432256b84105cb
trundler-dev <57276947+trundler-dev@users.noreply.github.com>
2021-12-06 19:48:49 -0400

    Chapter 3: Initial commit

--- code/MyFirstGame.cs
+++ code/MyFirstGame.cs
@@ -1,18 +1,24 @@
+using MyFirstGame.UI;
 using Sandbox;
 
 namespace MyFirstGame
 {
 	public partial class MyFirstGame : Game
 	{
+		public Hud Hud { get; set; }
+
 		public MyFirstGame()
 		{
 			Log.Info( "Hello from MyFirstGame!" );
+
+			if ( IsServer )
+				Hud = new Hud();
 		}
 
 		public override void ClientJoined( Client cl )
 		{
 			base.ClientJoined( cl );
-			
+
 			MyFirstPlayer player = new MyFirstPlayer();
 			cl.Pawn = player;
 
---
+++ code/UI/Hud.cs
@@ -0,0 +1,23 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+namespace MyFirstGame.UI
+{
+	public partial class Hud : HudEntity<RootPanel>
+	{
+		public Hud()
+		{
+			if ( !IsClient )
+				return;
+
+			// Option 1: HTML
+			// RootPanel.SetTemplate( "UI/Hud.html" );
+
+			// Option 2: C#
+			RootPanel.StyleSheet.Load( "UI/Style.scss" );
+			var myLabel = RootPanel.AddChild<Label>();
+			myLabel.Text = "My First Game";
+			// myLabel.Style.BackgroundColor = Color.Red;
+		}
+	}
+}
